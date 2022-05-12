0052a1c81b3f2e963fe40757ab4c94c467653518
eralash911 <unreal725@gmail.com>
2022-05-12 16:26:13 +0300

    some fix

---
+++ src/main/java/Netty/AbstractMessageHandler.java
@@ -0,0 +1,46 @@
+package Netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+import model.AbstractMessage;
+import model.FileMessage;
+import model.FileRequest;
+import model.FilesList;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
+public class AbstractMessageHandler extends SimpleChannelInboundHandler<AbstractMessage> {
+    private Path currentPath;
+
+    public AbstractMessageHandler() {
+        currentPath = Paths.get("A");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(new FilesList(currentPath));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx,
+                                AbstractMessage message) throws Exception {
+        log.debug("received: {}", message);
+        switch (message.getMessageType()){
+            case FILE:
+                FileMessage fileMessage = (FileMessage) message;
+                Files.write(currentPath.resolve(fileMessage.getFileName()),
+                        fileMessage.getBytes());
+                ctx.writeAndFlush(new FilesList(currentPath));
+                break;
+            case FILE_REQUEST:
+                FileRequest req = (FileRequest) message;
+                ctx.writeAndFlush(new FileMessage(currentPath.resolve(req.getFileName())));
+                break;
+        }
+    }
+}
---
+++ src/main/java/Netty/UserNameService.java
@@ -0,0 +1,22 @@
+package Netty;
+
+public class UserNameService {
+
+    private int cnt;
+
+    public UserNameService(){
+        cnt = 0;
+    }
+
+    public void userConnect(){
+        cnt++;
+    }
+    public void userDisconnect(){
+        cnt--;
+    }
+
+    public String getUserName(){
+      return "User# " + cnt;
+
+    }
+}
